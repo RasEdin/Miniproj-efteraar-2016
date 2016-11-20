11e46924de568928a8e9e9956c7766cd370c1d3c
Rasmus laptop <Rasmus laptop@192.168.0.20>
2016-11-20 23:43:35 +0100

    update

---
+++ src/model/Biotrio.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.*;
+
+/**
+ *
+ * @author Rasmus laptop
+ */
+public class Biotrio {
+    ArrayList<User> users = new ArrayList<>();
+    ArrayList<Film> films = new ArrayList<>();
+    ArrayList<Forestilling> forestillinger = new ArrayList<>();
+    ArrayList<Sal> sale = new ArrayList<>();
+
+    public Biotrio() {
+    }
+
+    
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
+
+    public ArrayList<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(ArrayList<Film> films) {
+        this.films = films;
+    }
+
+    public ArrayList<Forestilling> getForestillinger() {
+        return forestillinger;
+    }
+
+    public void setForestillinger(ArrayList<Forestilling> forestillinger) {
+        this.forestillinger = forestillinger;
+    }
+
+    public ArrayList<Sal> getSale() {
+        return sale;
+    }
+
+    public void setSale(ArrayList<Sal> sale) {
+        this.sale = sale;
+    }
+    
+    
+    
+}
