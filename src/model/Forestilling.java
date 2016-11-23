36b377efeacdcea706c8025e2e856f346ed220b5
slmns <slmns@10.200.124.26>
2016-11-23 13:00:17 +0100

    Merge origin/master
    Conflicts:
    	src/githubtest/Main.java

--- src/model/Forestilling.java
+++ src/model/Forestilling.java
@@ -5,31 +5,56 @@
  */
 package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import view.GUI_projekt;
 /**
  *
  * @author Rasmus laptop
  */
 public class Forestilling {
+
     private String tidspunkt;
     private int id;
     private Film[] film;
     private Sal sal;
-
-    
+    private Connection con;
+    private Statement stat = null;
+    ResultSet rs = null;
+    private PreparedStatement pst;
 
     public Forestilling(String tidspunkt, Film[] film, Sal sal) {
         this.tidspunkt = tidspunkt;
         this.film = film;
         this.sal = sal;
     }
+
     public Forestilling(String tidspunkt, int id, Film[] film, Sal sal) {
         this.tidspunkt = tidspunkt;
         this.id = id;
         this.film = film;
         this.sal = sal;
     }
-    
+
     public String getTidspunkt() {
+        try {
+
+            String tidspunkt = "insert into film(dato) values(?)";
+            pst = con.prepareStatement(tidspunkt);
+
+            pst.setString(1, ((JTextField)jDateChooser1.getDateEditor().getUiComponent()).getText());
+
+            pst.executeUpdate();
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+
+        }
         return tidspunkt;
     }
 
@@ -46,6 +71,16 @@ public class Forestilling {
     }
 
     public Sal getSal() {
+         try {
+
+            String sal = "insert into film(sal) values(?)";
+            pst = con.prepareStatement(sal);            
+            pst.setInt(1, Integer.parseInt(jComboBox1.getSelectedItem().toString()));
+           pst.executeUpdate();           
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+
+        }
         return sal;
     }
 
@@ -58,7 +93,4 @@ public class Forestilling {
         return "Forestilling{" + "tidspunkt=" + tidspunkt + ", id=" + id + ", film=" + film + ", sal=" + sal + '}';
     }
 
-    
-    
-    
 }
